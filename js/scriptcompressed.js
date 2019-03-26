e498fc91864e848a807ec20671930449c6a0e413
Viii <alis.donnikowa@yandex.ru>
2019-03-26 11:23:14 +0300

    Защита/3

--- js/scriptcompressed.js
+++ js/scriptcompressed.js
@@ -1,5 +1,4 @@
-var
-    search = document.querySelector(".search"),
+var search = document.querySelector(".search"),
     popup = document.querySelector(".popup"),
     enter = popup.querySelector(".enter"),
     exist = popup.querySelector(".exist"),
@@ -7,22 +6,31 @@ var
     inputenter = popup.querySelector(".enter"),
     inputexist = popup.querySelector(".exist");
 search.addEventListener("click", function(e) {
-        e.preventDefault(),
-            popup.classList.toggle("popup-show"),
-            enter.focus(),
-            inputenter.classList.remove("popup-error"),
-            inputexist.classList.remove("popup-error-two")
-    });
-    form.addEventListener("submit", function(e) {
-        enter.value && exist.value || (
-            e.preventDefault(),
-            inputenter.classList.add("popup-error"),
-            inputexist.classList.add("popup-error-two"))
-    });
-    window.addEventListener("keydown", function(e) {
-        27 === e.keyCode && (e.preventDefault(),
-            popup.classList.contains("popup-show") && (popup.classList.remove("popup-show"),
-                inputenter.classList.remove("popup-error"),
-                inputexist.classList.remove("popup-error-two")))
-    });
+    e.preventDefault(), popup.classList.toggle("popup-show"), enter.focus(), inputenter.classList.remove("popup-error"), inputexist.classList.remove("popup-error-two")
+});
+form.addEventListener("submit", function(e) {
+    enter.value && exist.value || (e.preventDefault(), inputenter.classList.add("popup-error"), inputexist.classList.add("popup-error-two"))
+});
+window.addEventListener("keydown", function(e) {
+    27 === e.keyCode && (e.preventDefault(), popup.classList.contains("popup-show") && (popup.classList.remove("popup-show"), inputenter.classList.remove("popup-error"), inputexist.classList.remove("popup-error-two")))
+});
+var mapClick = document.querySelector(".callmap"),
+    mapShow = document.querySelector(".map");
+mapClick.addEventListener("click", function(e) {
+    e.preventDefault(), mapShow.classList.toggle("map-show")
+});
+window.addEventListener("keydown", function(e) {
+    27 === e.keyCode && (e.preventDefault(), mapShow.classList.contains("map-show") && (mapShow.classList.remove("map-show")))
+})
   
+
+
+
+
+
+
+
+
+
+
+
